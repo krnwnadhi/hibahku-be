0d1c26d41d91a65dfebb357bd83516898e2c0682
Adhi Kurniawan <adhikurniawan2108@gmail.com>
2025-07-22 10:06:44 +0700

    Update: Change roleCheck code logic

--- middleware/roleCheck.js
+++ middleware/roleCheck.js
@@ -1,16 +1,11 @@
-const { User } = require("../models");
-
 const roleCheck = async (req, res, next) => {
-    const { user } = req; // Ganti "User" dengan "user"
-    console.log("Role ID:", user.roleid);
-    if (user.roleid !== 1) {
-        // Ganti "User" dengan "user", dan ubah operator perbandingan dari "!=" menjadi "!==" untuk perbandingan tipe data yang tepat
-        // console.log("Unauthorized - Role is not ADMIN");
-        return res.status(403).json({
-            message: "Maaf hanya Admin yang bisa mengakses halaman ini!",
-        });
+    if (req.user && req.user.roleid === 1) {
+        return next();
     }
-    next();
+
+    return res.status(403).json({
+        message: "Maaf, hanya Admin yang bisa mengakses halaman ini!",
+    });
 };
 
 module.exports = {
